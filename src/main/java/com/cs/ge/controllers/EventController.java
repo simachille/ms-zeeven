2482da9bf95b128c5cc7f229fdc2eb93598f637e
Achille MBOUGUENG <achille.mbougueng@yahoo.fr>
2022-06-11 13:31:49 +0200

    cors

--- src/main/java/com/cs/ge/controllers/EventController.java
+++ src/main/java/com/cs/ge/controllers/EventController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @AllArgsConstructor
 @RestController
 @RequestMapping(path = "event", produces = "application/json")
